9e22dcc80ec3010f4495e2e106a11ca0e81a20ae
sober-baby <61091443+sober-baby@users.noreply.github.com>
2021-12-09 13:28:16 -0500

    lol

--- TestCode/QuickSort.java
+++ TestCode/QuickSort.java
@@ -1,43 +1,43 @@
-// program that sorts a array of integeres using quick sort
+/*
+* Description: Program that sorts an array of integeres using quick sort
+*/
 package TestCode;
 import java.util.*;
 public class QuickSort {
     public static void main(String[] args) {
 
-        //quick sort array
-        int[] array = {5,3,2,4,1};
-        quickSort(array, 0, array.length-1);
-        for(int i = 0; i<array.length; i++){
+        int[] array = {5,3,2,4,1,6,8}; //array to be sorted
+        quickSort(array, 0, array.length-1); //sort the array
+        for(int i = 0; i<array.length; i++){ //print the array
             System.out.println(array[i]);
         }
-        
-      
     }
     
+    //quick sort recursive method
     public static void quickSort(int[] array, int low, int high){
-        if(low < high){
-            int pivot = partition(array, low, high);
-            quickSort(array, low, pivot-1);
-            quickSort(array, pivot+1, high);
+        if(low < high){ //base case
+            int pivot = partition(array, low, high); //choose pivot and partition array 
+            quickSort(array, low, pivot-1); //sort the left half
+            quickSort(array, pivot+1, high); //sort the right half
         }
     }
 
+    //partition method
     public static int partition(int[] array, int low, int high){
-        int pivot = array[high];
-        int i = low - 1;
-        for(int j = low; j<high; j++){
-            if(array[j] <= pivot){
-                i++;
-                int temp = array[i];
-                array[i] = array[j];
+        int pivot = array[high]; //choose pivot
+        int i = low - 1; //index of smaller element
+        for(int j = low; j<high; j++){ //loop through array
+            if(array[j] <= pivot){ //if element is smaller than pivot
+                i++; //increment index of smaller element
+                int temp = array[i]; //swap elements
+                array[i] = array[j]; 
                 array[j] = temp;
             }
         }
-       
-        int temp = array[i+1];
-        array[i+1] = array[high];
-        array[high] = temp;
-        return i+1+1;
+        int temp = array[i+1]; //swap elements 
+        array[i+1] = array[high]; 
+        array[high] = temp; 
+        return i+1; //return index of pivot
     }
 }
 
---
+++ TestCode/SortName.java
@@ -0,0 +1,41 @@
+/*
+* Descrption: sort a array of names in acdecning order using quick sort
+*/
+package TestCode;
+public class SortName {
+    public static void main(String[] args) {
+        
+        String[] array = {"james", "aiden", "daniel", "ali", "david"}; //array of names
+        quickSort(array, 0, array.length - 1);  //call the sort method
+        for (int i = 0; i < array.length; i++) { //pring the array
+            System.out.println(array[i]);
+        }
+    }
+    
+    //quick sort recursive method
+    public static void quickSort(String[] array, int low, int high) {
+        if (low < high) { //base case
+            int pivot = partition(array, low, high); //get the pivot
+            quickSort(array, low, pivot - 1); //sort the low side
+            quickSort(array, pivot + 1, high); //sort the high side
+        }
+    }
+
+    //partition the array
+    public static int partition(String[] array, int low, int high) {
+        String pivot = array[high]; //get the pivot
+        int i = low - 1; //index of smaller element
+        for(int j = low; j<high; j++){ //loop through array
+            if(array[j].compareTo(pivot) < 0){ //if element is smaller than pivot
+                i++; //increment index of smaller element
+                String temp = array[i]; //swap elements
+                array[i] = array[j]; 
+                array[j] = temp;
+            }
+        }
+        String temp = array[i+1]; //swap elements 
+        array[i+1] = array[high]; 
+        array[high] = temp; 
+        return i+1; //return index of pivot
+    }
+}
